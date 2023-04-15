5679b6662910912e51dead667d4dd740c61e37f0
BotsheloRamela <ramelabotshelo12@gmail.com>
2023-04-15 16:51:40 +0200

    Moved ConnectionHandler into Server

--- Server.java
+++ Server.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
@@ -25,7 +29,6 @@ public class Server implements Runnable {
                 Socket client = server.accept();
                 ConnectionHandler handler = new ConnectionHandler(client);
                 connections.add(handler);
-                handler.addNewConnection(handler);
                 threadPool.execute(handler);
             }
         } catch (Exception e) {
@@ -45,6 +48,68 @@ public class Server implements Runnable {
         } catch (Exception e) {
             // TODO: handle exception
         }
+    }
+
+    public void broadcastMessage(String message) {
+        for (ConnectionHandler ch: connections) {
+            if (ch != null) {
+                ch.sendMessage(message);
+            }
+        }
+    }
+
+    class ConnectionHandler implements Runnable{
+
+        private Socket client;
+        private BufferedReader input;
+        private PrintWriter output;
+        private String nickname;
+    
+        public ConnectionHandler(Socket client) {
+            this.client = client;
+        }
+    
+        @Override
+        public void run() {
+            try {
+                output = new PrintWriter(client.getOutputStream(), true);
+                input = new BufferedReader(new InputStreamReader(client.getInputStream()));
+                output.println("Enter a nickname: ");
+                nickname = input.readLine();
+                System.out.println(nickname + " connected!");
+                broadcastMessage(nickname + " joined the room!");
+                String message;
+                while ((message = input.readLine()) != null) {
+                    if (message.startsWith("/quit" )) {
+                        broadcastMessage(nickname + " left the room!");
+                        shutdownClient();
+                    } else {
+                        broadcastMessage(nickname + ": " + message);
+                    } 
+                }
+                
+            } catch (Exception e) {
+                shutdownClient();
+            }
+        }
+    
+        public void sendMessage(String messaeg) {
+            output.println(messaeg);
+        }
+    
+    
+        public void shutdownClient() {
+            try {
+                input.close();
+                output.close();
+                if (!client.isClosed()) {
+                    client.close();
+                }
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+            }
+        }
+        
     }
 
     public static void main(String[] args) {
