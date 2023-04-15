be3974be36c7c568e19a97e2b1eb8032534a2c4a
BotsheloRamela <ramelabotshelo12@gmail.com>
2023-04-15 15:00:00 +0200

    Created ConnectionHandler constructor

--- ConnectionHandler.java
+++ ConnectionHandler.java
@@ -1,7 +1,16 @@
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class ConnectionHandler implements Runnable{
 
-    
+    private Socket client;
+    private BufferedReader input;
+    private PrintWriter output;
 
+    public ConnectionHandler(Socket client) {
+        this.client = client;
+    }
     @Override
     public void run() {
         // TODO Auto-generated method stub
