9dd06de77bd3b411296bb5d8d23cbea0e648b01f
BotsheloRamela <ramelabotshelo12@gmail.com>
2023-04-15 16:07:14 +0200

    Init input, client, output in Client

--- Client.java
+++ Client.java
@@ -1,9 +1,22 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class Client implements Runnable{
+    private Socket client;
+    private BufferedReader input;
+    private PrintWriter output;
 
     @Override
     public void run() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'run'");
+        try {
+            client = new Socket("127.0.0.1", 6666);
+            output = new PrintWriter(client.getOutputStream(), true);
+            input = new BufferedReader(new InputStreamReader(client.getInputStream()));
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
     }
     
 }
