ffaa956cfd4329ac8d1d140f956daac0130c5f0b
BotsheloRamela <ramelabotshelo12@gmail.com>
2023-04-15 16:59:18 +0200

    Server code refactored

--- Server.java
+++ Server.java
@@ -9,7 +9,6 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Server implements Runnable {
-
     private ArrayList<ConnectionHandler> connections;
     private ServerSocket server;
     private boolean done;
@@ -19,7 +18,7 @@ public class Server implements Runnable {
         connections = new ArrayList<>();
         done = false;
     }
-    
+
     @Override
     public void run() {
         try {
@@ -27,7 +26,7 @@ public class Server implements Runnable {
             threadPool = Executors.newCachedThreadPool();
             while (!done) {
                 Socket client = server.accept();
-                ConnectionHandler handler = new ConnectionHandler(client);
+                ConnectionHandler handler = new ConnectionHandler(client, this);
                 connections.add(handler);
                 threadPool.execute(handler);
             }
@@ -52,70 +51,15 @@ public class Server implements Runnable {
     }
 
     public void broadcastMessage(String message) {
-        for (ConnectionHandler ch: connections) {
+        for (ConnectionHandler ch : connections) {
             if (ch != null) {
                 ch.sendMessage(message);
             }
         }
     }
 
-    class ConnectionHandler implements Runnable{
-
-        private Socket client;
-        private BufferedReader input;
-        private PrintWriter output;
-        private String nickname;
-    
-        public ConnectionHandler(Socket client) {
-            this.client = client;
-        }
-    
-        @Override
-        public void run() {
-            try {
-                output = new PrintWriter(client.getOutputStream(), true);
-                input = new BufferedReader(new InputStreamReader(client.getInputStream()));
-                output.println("Enter a nickname: ");
-                nickname = input.readLine();
-                System.out.println(nickname + " connected!");
-                broadcastMessage(nickname + " joined the room!");
-                String message;
-                while ((message = input.readLine()) != null) {
-                    if (message.startsWith("/quit" )) {
-                        broadcastMessage(nickname + " left the room!");
-                        shutdownClient();
-                    } else {
-                        broadcastMessage(nickname + ": " + message);
-                    } 
-                }
-                
-            } catch (Exception e) {
-                shutdownClient();
-            }
-        }
-    
-        public void sendMessage(String messaeg) {
-            output.println(messaeg);
-        }
-    
-    
-        public void shutdownClient() {
-            try {
-                input.close();
-                output.close();
-                if (!client.isClosed()) {
-                    client.close();
-                }
-            } catch (IOException e) {
-                // TODO Auto-generated catch block
-            }
-        }
-        
-    }
-
     public static void main(String[] args) {
         Server server = new Server();
         server.run();
     }
-
 }
