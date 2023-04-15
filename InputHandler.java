b3620c9abe722f979129a24606194b10ba318c74
BotsheloRamela <ramelabotshelo12@gmail.com>
2023-04-15 16:08:32 +0200

    Created Input handler

---
+++ InputHandler.java
@@ -0,0 +1,9 @@
+public class InputHandler implements Runnable {
+
+    @Override
+    public void run() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'run'");
+    }
+    
+}
