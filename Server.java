bf5d08c611cd56368d65a1a7cf6d5e5e89fed50a
BotsheloRamela <ramelabotshelo12@gmail.com>
2023-04-15 15:07:07 +0200

    Store new connections

--- Server.java
+++ Server.java
@@ -1,13 +1,18 @@
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 
 public class Server implements Runnable {
 
+    private ArrayList<ConnectionHandler> connections;
+    
     @Override
     public void run() {
         try {
             ServerSocket server = new ServerSocket(6666);
             Socket client = server.accept();
+            ConnectionHandler handler = new ConnectionHandler(client);
+            connections.add(handler);
         } catch (Exception e) {
             // TODO: handle exception
         }
