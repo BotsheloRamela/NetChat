4f88ae3dec1243c234d85927bd5d39fa3b2ab954
BotsheloRamela <ramelabotshelo12@gmail.com>
2023-04-15 17:03:29 +0200

    Refactored code & moved InputHandler logic

--- Client.java
+++ Client.java
@@ -3,71 +3,22 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
-public class Client implements Runnable{
-    private Socket client;
-    private BufferedReader input;
-    private PrintWriter output;
-    private boolean done;
-
-    @Override
-    public void run() {
-        try {
-            client = new Socket("127.0.0.1", 6666);
-            output = new PrintWriter(client.getOutputStream(), true);
-            input = new BufferedReader(new InputStreamReader(client.getInputStream()));
-
-            InputHandler inHandler = new InputHandler();
-            Thread thread = new Thread(inHandler);
-            thread.start();
-
-            String inMessage;
-            while ((inMessage = input.readLine()) != null) {
-                System.out.println(inMessage);
-            }
-        } catch (Exception e) {
-            shutdown();
-        }
-    }
-
-    public void shutdown() {
-        done = true;
-        try {
-            input.close();
-            output.close();
-            if (!client.isClosed()) {
-                client.close();
-            }
-        } catch (Exception e) {
-            // TODO: handle exception
-        }
-    }
-
-    class InputHandler implements Runnable {
-    
-        @Override
-        public void run() {
-            
-            try {
-                BufferedReader inReader = new BufferedReader(new InputStreamReader(System.in));
-                while (!done) {
-                    String message = inReader.readLine();
-                    if (message.equals("/quit")) {
-                        output.println(message);
-                        inReader.close();
-                        shutdown();
-                    } else {
-                        output.println(message);
-                    }
-                }
-            } catch (Exception e) {
-                shutdown();
-            }
-        }
-    }
-
+public class Client{
     public static void main(String[] args) {
-        Client client = new Client();
-        client.run();
+        try {
+            Socket socket = new Socket("127.0.0.1", 6666);
+            BufferedReader socketInput = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter socketOutput = new PrintWriter(socket.getOutputStream(), true);
+            InputHandler inputHandler = new InputHandler(socketOutput);
+            Thread inputThread = new Thread(inputHandler);
+            inputThread.start();
+            String message;
+            while ((message = socketInput.readLine()) != null) {
+                System.out.println(message);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
     
 }
