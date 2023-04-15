69788ef83f7f9fac182b9f68162ccae5398ed4a3
BotsheloRamela <ramelabotshelo12@gmail.com>
2023-04-15 16:35:55 +0200

    Moved InputHandler into Client

--- Client.java
+++ Client.java
@@ -7,6 +7,7 @@ public class Client implements Runnable{
     private Socket client;
     private BufferedReader input;
     private PrintWriter output;
+    private boolean done;
 
     @Override
     public void run() {
@@ -17,6 +18,40 @@ public class Client implements Runnable{
         } catch (Exception e) {
             // TODO: handle exception
         }
+    }
+
+    class InputHandler implements Runnable {
+    
+        @Override
+        public void run() {
+            
+            try {
+                BufferedReader inReader = new BufferedReader(new InputStreamReader(System.in));
+                while (!done) {
+                    String message = inReader.readLine();
+                    if (message.equals("/quite")) {
+                        inReader.close();
+                        shutdown();
+                    }
+                }
+            } catch (Exception e) {
+                // TODO: handle exception
+            }
+        }
+    
+        public void shutdown() {
+            done = true;
+            try {
+                input.close();
+                output.close();
+                if (!client.isClosed()) {
+                    client.close();
+                }
+            } catch (Exception e) {
+                // TODO: handle exception
+            }
+        }
+        
     }
     
 }
--- InputHandler.java
+++
@@ -1,9 +0,0 @@
-public class InputHandler implements Runnable {
-
-    @Override
-    public void run() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'run'");
-    }
-    
-}
