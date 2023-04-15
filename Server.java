1a25d087c2583591021313eb6ba9d7d4e4ec2103
BotsheloRamela <ramelabotshelo12@gmail.com>
2023-04-15 15:24:09 +0200

    Moved broadcastMsg & changed new connection logic

--- Server.java
+++ Server.java
@@ -12,18 +12,10 @@ public class Server implements Runnable {
             ServerSocket server = new ServerSocket(6666);
             Socket client = server.accept();
             ConnectionHandler handler = new ConnectionHandler(client);
-            connections.add(handler);
+            ConnectionHandler.addNewConnection(handler);
         } catch (Exception e) {
             // TODO: handle exception
         }
     }
 
-    public void broadcastMessage(String message) {
-        for (ConnectionHandler ch: connections) {
-            if (ch != null) {
-                ch.sendMessage(message);
-            }
-        }
-    }
-    
 }
