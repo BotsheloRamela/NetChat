d0c409e0ad200c57c2e0ce117652fe33a27ca32b
BotsheloRamela <ramelabotshelo12@gmail.com>
2023-04-16 15:18:49 +0200

    Created secret key preparation method

--- AESEncryptionDecryption.java
+++ AESEncryptionDecryption.java
@@ -1,3 +1,25 @@
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+
+import javax.crypto.spec.SecretKeySpec;
+
 public class AESEncryptionDecryption {
-    
+    private static SecretKeySpec secretKey;
+    private static byte[] key;
+    private static final String ALGORITHM = "AES";
+
+    public void prepareSecretKey(String myKey) {
+        MessageDigest sha = null;
+        try {
+            key = myKey.getBytes(StandardCharsets.UTF_8);
+            sha = MessageDigest.getInstance("SHA-1");
+            key = sha.digest(key);
+            key = Arrays.copyOf(key, 16);
+            secretKey = new SecretKeySpec(key, ALGORITHM);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+    } 
 }
