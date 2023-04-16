a8218a48446d490df614a3f6224e2b955309e9d0
BotsheloRamela <ramelabotshelo12@gmail.com>
2023-04-16 17:10:13 +0200

    Added documentation

--- ClientConnection.java
+++ ClientConnection.java
@@ -3,16 +3,33 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
+/**
+    This class represents a connection between a client and a server.
+    It implements the Runnable interface to allow for it to be run on a separate thread.
+*/
+
+
 public class ClientConnection implements Runnable{
     private Socket socket;
     private BufferedReader input;
     private PrintWriter output;
     private boolean done;
 
+    /**
+        Constructor for a new ClientConnection object.
+        @param socket The socket representing the connection between the client and server.
+    */
+
     public ClientConnection(Socket socket) {
         this.socket = socket;
         done = false;
     }
+
+    /**
+        * This method is called when a new thread is started for this object.
+        * It initializes the input and output streams and listens for incoming messages from the server.
+        * When a message is received, it is printed to the console.
+    */
 
     @Override
     public void run() {
@@ -29,10 +46,17 @@ public class ClientConnection implements Runnable{
         }
     }
 
+    /**
+        This method sends a message to the server through the output stream.
+        @param message The message to be sent.
+    */
     public void sendMessage(String message) {
         output.println(message);
     }
 
+    /**
+        This method disconnects the client from the server by closing the input and output streams and the socket.
+    */
     public void disconnect() {
         done = true;
         try {
